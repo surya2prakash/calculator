62ad2f1f8f959a2dc087ce9081eb33624028846b
surya2prakash <sv251171@gmail.com>
2025-08-15 22:46:10 +0530

    styling is add

---
+++ src/Calculator.js
@@ -0,0 +1,71 @@
+
+import { evaluate } from 'mathjs';
+import React, { useState } from 'react'
+
+import './Calculator.css'
+
+export default function Calculator() {
+
+    const [value,setValue] =useState("");
+
+    function clickHandler(event){
+        // button ki value nikal lo
+
+        const buttonKiValue = event.target.innerText;
+
+        // ab check karo kahin vo value C to nhi ager c hogi to input me jo hoga usko clear krna pade ga
+         
+        if(buttonKiValue === 'C')
+        {
+            // output field ko clear kro
+            setValue("");
+        }else if(buttonKiValue === "=")
+        {
+            // ager equal hai to calculation kro --->
+          try{
+                setValue(evaluate(value).toString());
+          }catch{
+            setValue("Error !!")
+          }
+           
+        }else{
+            //es case me tumne ya to numbers pe click kiya hoga nhi to optators pe
+
+            setValue(value + buttonKiValue);
+        }
+
+    }
+
+
+  return (
+    <div className='main'>
+        {/* heading */}
+          <h2>Calculator</h2>
+          {/* output ke liye */}
+          <input type='text' className='result' value={value} readOnly/>
+
+          <div className='buttons_containers'>
+              
+              <button onClick={clickHandler} className='clear'>C</button>
+              <button onClick={clickHandler} className='di'>/</button>
+              <button onClick={clickHandler} className='mul'>*</button>
+              <button onClick={clickHandler} className='min' >-</button>
+              <button onClick={clickHandler}>7</button>
+              <button onClick={clickHandler}>8</button>
+              <button onClick={clickHandler}>9</button>
+              <button onClick={clickHandler} className='plus'>+</button>
+              <button onClick={clickHandler} >4</button>
+              <button onClick={clickHandler}>5</button>
+              <button onClick={clickHandler}>6</button>
+              <button onClick={clickHandler} className='equal'>=</button>
+              <button onClick={clickHandler }>1</button>
+              <button onClick={clickHandler}>2</button>
+              <button onClick={clickHandler} >3</button>
+              <button onClick={clickHandler}>0</button>
+              <button onClick={clickHandler} className='dot'>.</button>
+
+          </div>
+
+    </div>
+  )
+}
